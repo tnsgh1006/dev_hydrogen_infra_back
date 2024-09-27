4afc3664837b08e56d2c28e2cdce9094a52f3e5c
soonho <tnsgh1006@naver.com>
2024-09-27 16:02:07 +0900

    첫 등록

---
+++ db.js
@@ -0,0 +1,24 @@
+// backend/db.js
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+// .env 파일 로드
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // 환경 변수로 포트를 지정
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
+
+module.exports = connection;
